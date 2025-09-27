b14025f679dfcc970140db9611711f3bf5290397
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2025-09-27 07:34:10 +0530

    first commit

--- text-gray-slide/app/page.js
+++ text-gray-slide/app/page.js
@@ -1,103 +1,131 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
+import Lenis from "lenis"; 
+import { useEffect } from "react";
+import { useGSAP } from '@gsap/react';
+import { gsap } from "gsap";
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      duration: 2.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    const texts = document.querySelectorAll('.animate-text').forEach((text) => {
+      text.setAttribute('data-text', text.textContent.trim());
+
+      ScrollTrigger.create({
+        trigger: text, 
+        start: "top 50%", 
+        end: "bottom 50%", 
+        scrub: 1, 
+        onUpdate: (self) => {
+          const clipValue = Math.max(0, 100 - self.progress * 100); 
+          text.style.setProperty("--clip-value", `${clipValue}%`);
+        }
+      })
+    }); 
+
+    ScrollTrigger.create({
+      trigger: ".services", 
+      start: "top bottom", 
+      end: "top top", 
+      scrub: 1, 
+      onUpdate: (self) => {
+        const headers = document.querySelectorAll('.services-header'); 
+        gsap.set(headers[0], { x: `${100 - self.progress * 100}%`}); 
+        gsap.set(headers[1], { x: `${-100 + self.progress * 100}%`});
+        gsap.set(headers[2], { x: `${100 - self.progress * 100}%`});
+      },
+    });
+
+    ScrollTrigger.create({
+      trigger: ".services", 
+      start: "top top", 
+      end: `+=${window.innerHeight * 2}`,
+      pin: true, 
+      scrub: 1, 
+      pinSpacing: false, 
+      onUpdate: (self) => {
+        const headers = document.querySelectorAll('.services-header'); 
+
+        if(self.progress <= 0.5){
+          const yProgress = self.progress / 0.5; 
+          gsap.set(headers[0], { y: `${100 * yProgress}%`});
+          gsap.set(headers[2], { y: `${-100 * yProgress}%`});
+        }else{
+          gsap.set(headers[0], { y: `100%`});
+          gsap.set(headers[2], { y: `-100%`});
+
+          const scaleProgress = (self.progress - 0.5) / 0.5; 
+          const minScale = window.innerWidth <= 1000 ? 0.3 : 0.1; 
+          const scale = 1 - scaleProgress * ( 1 - minScale); 
+
+          headers.forEach((headers) => gsap.set(headers, { scale })); 
+
+        }
+      }
+    })
+
+  }, []);
+
+
+  return (
+    <>
+      <section class="hero">
+        <img class="hero-img" src="work_01.jpg" />
+      </section>
+      <section class="about">
+        <h1 className="animate-text">
+          A space for work shaped with clarity and intention. Each project
+          follows a simple path from thought to form, from form to function.
+        </h1>
+      </section>
+
+      <section class="services">
+        <div className="services-header">
+          <img src="/W.png" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="services-header">
+          <img src="/W.png" />
+        </div>
+        <div className="services-header">
+          <img src="/W.png" />
+        </div>
+      </section>
+
+      <section class="services-copy">
+        <h1 class="animate-text">
+          We design and build digital products, brands, and experiences. This
+          includes UI/UX design, front-end development, and branding.
+        </h1>
+      </section>
+
+      <section class="outro">
+        <img class="outro-img" src="work_01.jpg" />
+      </section>
+    </>
   );
 }
